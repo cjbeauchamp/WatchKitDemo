c46ed844b5c9c7d0f43d5928c67bf2f3f31b4a01
Chris Beauchamp <cjbeauchamp@gmail.com>
2015-03-26 17:15:02 -0400

    assertion fix

--- WatchKitDemo/WatchKitDemo/AppDelegate.m
+++ WatchKitDemo/WatchKitDemo/AppDelegate.m
@@ -28,7 +28,7 @@ handleWatchKitExtensionRequest:(NSDictionary *)userInfo
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
-    NSString *critterAppID = @"YOUR_APP_ID_GOES_HEREE";
+    NSString *critterAppID = @"YOUR_APP_ID_GOES_HERE";
     
     NSAssert(![critterAppID isEqualToString:@"YOUR_APP_ID_GOES_HERE"], @"Enter your Crittercism App ID into the app delegate variable critterAppID!");
     
