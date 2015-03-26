f7717ec1ed5eb6e92aeccbba9ba6036c6e517984
Chris Beauchamp <cjbeauchamp@gmail.com>
2015-03-26 09:26:39 -0400

    added target

---
+++ WatchKitDemo/WatchKitDemo WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  WatchKitDemo WatchKit Extension
+//
+//  Created by Chris Beauchamp on 3/26/15.
+//  Copyright (c) 2015 Crittercism. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
---
+++ WatchKitDemo/WatchKitDemo WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  WatchKitDemo WatchKit Extension
+//
+//  Created by Chris Beauchamp on 3/26/15.
+//  Copyright (c) 2015 Crittercism. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
