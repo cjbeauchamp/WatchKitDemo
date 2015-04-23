9955a104b673848faa416ad810511c2db050b987
Chris Beauchamp <cjbeauchamp@gmail.com>
2015-04-23 18:40:16 -0400

    end tutorial 2

--- WatchKitDemo/WatchKitDemo WatchKit Extension/GlanceController.m
+++ WatchKitDemo/WatchKitDemo WatchKit Extension/GlanceController.m
@@ -8,28 +8,65 @@
 
 #import "GlanceController.h"
 
+#import "Crittercism.h"
 
-@interface GlanceController()
+@interface GlanceController(){
+    NSTimer *_updateTimer;
+}
 
 @end
 
-
 @implementation GlanceController
+
+- (void) requestSpeedFromPhone
+{
+    [WKInterfaceController openParentApplication:@{@"request":@"location"}
+                                           reply:^(NSDictionary *replyInfo, NSError *error) {
+                                               
+                                               // the request was successful
+                                               if(error == nil) {
+                                                   
+                                                   // get the serialized location object
+                                                   NSDictionary *location = replyInfo[@"location"];
+                                                   
+                                                   // pull out the speed (it's an NSNumber)
+                                                   NSNumber *speed = location[@"speed"];
+                                                   
+                                                   // format it nicely for display
+                                                   NSString *speedString = [NSString stringWithFormat:@"%.2lf", speed.doubleValue];
+                                                   
+                                                   // update our label with the newest location's speed
+                                                   [_glanceSpeed setText:speedString];
+                                               }
+                                               
+                                               // the request failed
+                                               else {
+                                                   [Crittercism logError:error];
+                                               }
+                                           }];
+}
 
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
-
-    // Configure interface objects here.
 }
 
 - (void)willActivate {
+    
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
+    
+    _updateTimer = [NSTimer scheduledTimerWithTimeInterval:1.f
+                                                    target:self
+                                                  selector:@selector(requestSpeedFromPhone)
+                                                  userInfo:nil
+                                                   repeats:TRUE];
 }
 
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
+    
+    [_updateTimer invalidate];
 }
 
 @end
