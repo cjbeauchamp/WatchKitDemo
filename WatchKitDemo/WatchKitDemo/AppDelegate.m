2866eb988972e13fd3067a16db2798158799cf29
Chris Beauchamp <cjbeauchamp@gmail.com>
2015-04-24 14:17:31 -0400

    patch

--- WatchKitDemo/WatchKitDemo/AppDelegate.m
+++ WatchKitDemo/WatchKitDemo/AppDelegate.m
@@ -28,7 +28,7 @@ handleWatchKitExtensionRequest:(NSDictionary *)userInfo
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
-    NSString *critterAppID = @"54187367d478bc786e00000e";
+    NSString *critterAppID = @"YOUR_APP_ID_GOES_HERE";
     
     NSAssert(![critterAppID isEqualToString:@"YOUR_APP_ID_GOES_HERE"], @"Enter your Crittercism App ID into the app delegate variable critterAppID!");
     
